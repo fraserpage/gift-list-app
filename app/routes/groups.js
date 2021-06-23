4c0ddd89a41f1880d9639394d1c87242faa1f3d8
fraserpage <fraserpage@gmail.com>
2021-06-23 10:21:15 -0400

    refactor route

--- app/routes/groups.js
+++ app/routes/groups.js
@@ -4,10 +4,11 @@ const groupCtrl = require('../controllers/groups')
 
 /* /groups */
 router.get('/', groupCtrl.index)
-router.get('/new', groupCtrl.new)
+// router.get('/new', isLoggedIn, groupCtrl.new)
 router.get('/:id', groupCtrl.show)
 
 router.post('/', groupCtrl.create)
 router.put('/:id', groupCtrl.update)
+
 
 module.exports = router
