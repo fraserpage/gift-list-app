c2f98a9cf11e7035db0689c6d33c64ac10648a62
fraserpage <fraserpage@gmail.com>
2021-06-24 11:01:15 -0400

    add invite logic to login

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,12 @@
 var router = require('express').Router();
 const passport = require('passport');
+const Group = require('../models/group')
 
 // The root route renders our only view
 router.get('/login', function(req, res) {
-  res.render('login',{
+  res.render('message',{
     title: 'Login',
+    message: "You'll need to login before you can see that page.",
     user: req.user,
   });
 });
@@ -19,10 +21,32 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successReturnToOrRedirect : '/',
+    // successReturnToOrRedirect : '/',
     failureRedirect : '/'
   }
-));
+),async function(req, res) {
+  // Successful authentication, redirect home.
+
+  console.log('session',req.session.invite)
+  // if this was login in response to invite
+  if (req.session.invite){
+    const group = await Group.findById(req.session.invite.group)
+    const invite = group.invites.id(req.session.invite.invite)
+    delete req.session.invite
+    if (invite){
+      console.log('group before',group)
+      group.users.push(req.user._id)
+      invite.remove()
+      await group.save()
+      console.log('group after',group)
+    }
+    // do we want to send a flash message here?
+    res.redirect('/groups/'+req.params.id)
+  }
+  else{
+    res.redirect('/');
+  }
+});
 
 // OAuth logout route
 router.get('/logout', function(req, res){
