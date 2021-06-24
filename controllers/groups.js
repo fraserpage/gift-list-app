6962d12bdce18b2d643a360757e50098cd705827
fraserpage <fraserpage@gmail.com>
2021-06-24 10:58:35 -0400

    add mail

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,4 +1,5 @@
 const Group = require('../models/group')
+const sendMail = require('../mail/sendMail')
 
 module.exports = { index, show, new: newGroup, create, update }
 
@@ -13,7 +14,7 @@ async function index(req,res){
 }
 
 async function show(req,res){
-  const group = await Group.findById(req.params.id).populate('giftLists.user').exec()
+  const group = await Group.findById(req.params.id).populate('owner').populate('invites').populate('giftLists.user').exec()
   res.render('groups/show',{
     title:'group', 
     user: req.user,
@@ -22,15 +23,20 @@ async function show(req,res){
 }
 
 function newGroup(){
-  res.send('new group form')
+  res.send('new group form... currently showing the new form on the index page')
 }
 
 async function create(req,res){
-  req.body.invites = req.body.invites.replace(/\s/g,'').split(",")
   req.body.owner = req.user._id
   req.body.users = [req.user._id]
   try {
     const group = await Group.create(req.body)
+    // send email invites
+    for (let invite of group.invites){
+      sendMail.invite(group, invite, req.user, req.body)
+      
+    }
+
     res.redirect('/groups/'+group._id)
 
   } catch (error) {
@@ -39,6 +45,10 @@ async function create(req,res){
   }
 }
 
-function update(){
-  res.send('update a group')
+async function update(req, res){
+  const group = await Group.findById(req.params.id)
+  if (req.name) group.name = req.name
+  if (req.desc) group.desc = req.desc
+
 }
+
