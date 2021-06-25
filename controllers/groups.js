eff92f3af7d7eda9e56bb23fccee7660af2da496
fraserpage <fraserpage@gmail.com>
2021-06-24 20:55:21 -0400

    add edit fn

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,11 +1,18 @@
 const Group = require('../models/group')
 const sendMail = require('../mail/sendMail')
 
-module.exports = { index, show, new: newGroup, create, update }
+module.exports = { 
+  index, 
+  show, 
+  new: newGroup, 
+  edit,
+  invite,
+  create, update 
+}
 
 async function index(req,res){
     // find groups that the user is a member of
-    const groups = await Group.find({ users: req.user._id })
+    const groups = await Group.find({ users: req.user._id }).populate('users')
     res.render('groups/index',{
       title:'groups', 
       user: req.user,
@@ -14,7 +21,13 @@ async function index(req,res){
 }
 
 async function show(req,res){
-  const group = await Group.findById(req.params.id).populate('owner').populate('invites').populate('giftLists.user').exec()
+  const group = await Group.findById(req.params.id)
+  .populate('users')
+  .populate('owner')
+  .populate('invites')
+  .populate('giftLists.user')
+  .populate('giftLists.gifts.claimedBy')
+  .exec()
   res.render('groups/show',{
     title:'group', 
     user: req.user,
@@ -44,10 +57,35 @@ async function create(req,res){
   }
 }
 
+async function edit(req,res){
+  const group = await Group.findById(req.params.id).populate('owner').populate('invites').populate('giftLists.user').exec()
+  res.render('groups/edit',{
+    title:'group', 
+    user: req.user,
+    group,
+  })
+}
+
 async function update(req, res){
   const group = await Group.findById(req.params.id)
-  if (req.name) group.name = req.name
-  if (req.desc) group.desc = req.desc
+  if (req.body.name) group.name = req.body.name
+  if (req.body.desc) group.desc = req.body.desc
+  await group.save()
+  res.redirect('/groups/'+req.params.id)
+}
 
+async function invite(req, res){
+  let group = await Group.findById(req.params.id)
+  req.body.invites = req.body.invites.filter(e=>e.email)
+  if (req.body.invites.length){
+    group.invites.push(...req.body.invites)
+    group = await group.save()
+    console.log(group)
+    for (let invite of req.body.invites){
+      let inviteObj = group.invites.find(i=>i.email === invite.email)
+      sendMail.invite(group, inviteObj, req.user, req.body)
+    }
+  }
+  res.redirect('/groups/'+req.params.id)
 }
 
